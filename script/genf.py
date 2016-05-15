6bf8b774d648a1f9af00b52227ce386dfa8dbd12
Dan Bikle <bikle@bikle.com>
2016-05-15 22:44:52 +0000

    more

--- script/genf.py
+++ script/genf.py
@@ -32,33 +32,39 @@ cp_a = df1[['cp']].values
 # cp should be a list sorted by date-ascending:
 cp   = [elm[0] for elm in reversed(cp_a)]
 
-cplead_l = cp + [cp[-1]]
-cplag1_l = [cp[0]] + cp
-cplag2_l = [cp[0],cp[0]]             + cp
-cplag4_l = [cp[0],cp[0],cp[0],cp[0]] + cp
-cplag8_l = [cp[0],cp[0],cp[0],cp[0]] + cplag4_l
+cplead_l  = cp + [cp[-1]]
+cplag1_l  = [cp[0]] + cp
+cplag2_l  = [cp[0],cp[0]]             + cp
+cplag4_l  = [cp[0],cp[0],cp[0],cp[0]] + cp
+cplag8_l  = [cp[0],cp[0],cp[0],cp[0]] + cplag4_l
+cplag16_l = [cp[0]]*16 + cp
+
+
 # I should snip off ends so new columns as long as cp:
-cplead_l = cplead_l[1:]
-cplag1_l = cplag1_l[:-1]
-cplag2_l = cplag2_l[:-2]
-cplag4_l = cplag4_l[:-4]
-cplag8_l = cplag8_l[:-8]
+cplead_l  = cplead_l[1:]
+cplag1_l  = cplag1_l[:-1]
+cplag2_l  = cplag2_l[:-2]
+cplag4_l  = cplag4_l[:-4]
+cplag8_l  = cplag8_l[:-8]
+cplag16_l = cplag16_l[:-16]
 
 # NumPy allows me to do arithmetic on its Arrays.
 # I should convert my lists to Arrays:
-cp_a     = np.array(cp)
-cplead_a = np.array(cplead_l)
-cplag1_a = np.array(cplag1_l)
-cplag2_a = np.array(cplag2_l)
-cplag4_a = np.array(cplag4_l)
-cplag8_a = np.array(cplag8_l)
+cp_a      = np.array(cp)
+cplead_a  = np.array(cplead_l)
+cplag1_a  = np.array(cplag1_l)
+cplag2_a  = np.array(cplag2_l)
+cplag4_a  = np.array(cplag4_l)
+cplag8_a  = np.array(cplag8_l)
+cplag16_a = np.array(cplag16_l)
 
 # I should calculate pct-deltas:
-pctlead_a = 100.0 * (cplead_a - cp_a)/cp_a
-pctlag1_a = 100.0 * (cp_a - cplag1_a)/cplag1_a
-pctlag2_a = 100.0 * (cp_a - cplag2_a)/cplag2_a
-pctlag4_a = 100.0 * (cp_a - cplag4_a)/cplag4_a
-pctlag8_a = 100.0 * (cp_a - cplag8_a)/cplag8_a
+pctlead_a  = 100.0 * (cplead_a - cp_a)/cp_a
+pctlag1_a  = 100.0 * (cp_a - cplag1_a)/cplag1_a
+pctlag2_a  = 100.0 * (cp_a - cplag2_a)/cplag2_a
+pctlag4_a  = 100.0 * (cp_a - cplag4_a)/cplag4_a
+pctlag8_a  = 100.0 * (cp_a - cplag8_a)/cplag8_a
+pctlag16_a = 100.0 * (cp_a - cplag16_a)/cplag16_a
 
 # I am done doing calculations.
 # I should put my columns into a DataFrame.
@@ -68,11 +74,12 @@ df2         = pd.DataFrame(cdate_l)
 df2.columns = ['cdate']
 df2['cp']   = cp
 
-df2['pctlead'] = pctlead_a
-df2['pctlag1'] = pctlag1_a
-df2['pctlag2'] = pctlag2_a
-df2['pctlag4'] = pctlag4_a
-df2['pctlag8'] = pctlag8_a
+df2['pctlead']  = pctlead_a
+df2['pctlag1']  = pctlag1_a
+df2['pctlag2']  = pctlag2_a
+df2['pctlag4']  = pctlag4_a
+df2['pctlag8']  = pctlag8_a
+df2['pctlag16'] = pctlag16_a
 
 # I should save my work into a CSV file.
 # My input file should look something like this:
