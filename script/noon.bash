9f6f06a6fb1b7064309368cf3231179c911d968a
Dan Bikle <bikle@bikle.com>
2016-05-17 16:47:35 -0700

    more

---
+++ script/noon.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# ~/spy611v2/script/noon.bash
+
+# I should run this at 12:50 Calif time.
+
+# Demo:
+# ${HOME}/spy611v2/script/dan.bash ./noon.bash
+
+export     SPY611=${HOME}/spy611v2
+mkdir -p ${SPY611}/public/csv/
+cd       ${SPY611}/public/csv/
+
+# I should get prices
+echo ${SPY611}/script/wgetGSPC.bash
+
+# I should generate features from prices:
+${HOME}/anaconda3/bin/python ${SPY611}/script/genf.py GSPC2.csv
+
+# I should generate training and test data from features.
+
+# Next I generate training data CSV files:
+rm -rf   /tmp/ddata/
+mkdir -p /tmp/ddata/
+cd       /tmp/ddata/
+cp ${SPY611}/public/csv/ftrGSPC2.csv /tmp/ddata/
+STARTYR=2015
+ENDYR=2016
+${HOME}/anaconda3/bin/python ${SPY611}/script/gentrain_test.py ftrGSPC2.csv 30 $STARTYR $ENDYR
+
+# I should learn then test
+${HOME}/anaconda3/bin/python ${SPY611}/script/train_test.py $STARTYR $ENDYR
+# Now I should have CSV files with predictions mixed with actual results.
+
+exit
