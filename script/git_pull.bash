bc46bf6e7a71d946684a55d6331cbfd30a58e4f9
Dan Bikle <bikle@bikle.com>
2016-05-18 21:30:15 -0700

    more

--- script/git_pull.bash
+++ script/git_pull.bash
@@ -2,9 +2,11 @@
 
 # git_pull.bash
 
-# This script should do a simple git pull from github.
+# This script should do a simple git pull from github, bit, heroku.
 # I intend for this to run from cron twice a day.
 export  SPY611=${HOME}/spy611v2
 cd     $SPY611
-git pull gh master
+git pull gh     master
+git pull bit    master
+git pull heroku master
 exit
