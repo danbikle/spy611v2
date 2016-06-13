1ee3457cae8bc682c880d1d4512702be436f0bca
Dan Bikle <bikle@bikle.com>
2016-06-12 17:52:35 -0700

    git_push.bash.commit

--- script/git_push.bash
+++ script/git_push.bash
@@ -5,12 +5,12 @@
 # This script should push data,files to gh,bit,heroku.
 
 # I like to pull before I push.
-git pull gh master
-git pull bit master
+#git pull gh master
+#git pull bit master
 git pull heroku master
 git add .
 git commit -am git_push.bash.commit
-git push gh master
-git push bit master
+#git push gh master
+#git push bit master
 git push heroku master
 exit
