ba6709e7144afdaf5b344495d533f27f19aaab87
Dan Bikle <bikle@bikle.com>
2017-05-18 14:14:44 -0700

    more

--- script/wgetGSPCnight.bash
+++ script/wgetGSPCnight.bash
@@ -11,12 +11,10 @@
 mkdir -p ${HOME}/spy611/public/csv/
 cd       ${HOME}/spy611/public/csv/
 
-TKRH='%5EGSPC'
 TKR='GSPC'
-rm -f ${TKR}.csv
+rm -f ${TKR}2.csv
 
-wget --output-document=/tmp/${TKR}.csv http://ichart.finance.yahoo.com/table.csv?s=${TKRH}
-echo 'cdate,cp'                                           > ${TKR}2.csv
-grep -v Date /tmp/${TKR}.csv|awk -F, '{print $1 "," $5}' >> ${TKR}2.csv
+wget --output-document=${TKR}2.csv http://tkrapi2.herokuapp.com/static/tkrs/gspc.csv
+#ichart.finance.yahoo.com/table.csv?s=${TKRH}
 
 exit
