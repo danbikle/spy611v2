517ba780e69302f4a578b7c0c61e402a69a76864
Dan Bikle <bikle@bikle.com>
2017-06-28 14:27:48 -0700

    more

--- script/dedupGSPC2csv.py
+++ script/dedupGSPC2csv.py
@@ -9,7 +9,15 @@ import pandas as pd
 
 prices_df = pd.read_csv('GSPC2.csv')
 gb_df     = prices_df.groupby(['cdate']).cp.mean()
+# I should now have dedupped data but last price might be wrong.
 
+# I should get a very last price I trust:
+lastprice_f = prices_df.cp.iloc[-1]
+
+# I should update gb_df with better last price:
+gb_df.iloc[-1] = lastprice_f
+
+# I should write the dedupped data to csv:
 gb_df.to_csv('/tmp/gb.csv', float_format='%4.3f',index_label='cdate',header=True)
 
 'bye'
